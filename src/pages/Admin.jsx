65cba54df7d9d90dd7dfd2a76f199303dc22a60f
mumuniazeez <mumuniazeez99@gmail.com>
2024-12-07 00:46:06 +0100

    `Refactored Admin page layout and added LoadingAnimation component`

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -10,6 +10,7 @@ import AddAdminModal from "../components/AddAdminModal";
 import usePrompt from "../hooks/usePrompt";
 import useAlert from "../hooks/useAlert";
 import { useNavigate } from "react-router-dom";
+import LoadingAnimation from "../components/LoadingAnimation";
 
 const Admin = () => {
   const [myProfile, setMyProfile] = useState(null);
@@ -64,80 +65,76 @@ const Admin = () => {
       }
     );
   };
-  return (
-    myProfile &&
-    admins && (
-      <>
-        <div className="container p-5">
-          <div className="row py-3 px-3 align-items-center">
-            <div className="col-1">
-              <div className="project--text--primary">
-                <FontAwesomeIcon icon={faUser} style={{ fontSize: 91 }} />
-              </div>
-            </div>
-            <div className="col-7 ps-4">
-              <h1>
-                {myProfile.firstName} {myProfile.lastName}
-              </h1>
-              <p>{myProfile.email}</p>
-            </div>
-            <div className="col-4">
-              <button className="btn project--text--bg--primary me-3">
-                Edit Profile
-              </button>
-              <button className="btn btn-danger" onClick={logOut}>
-                <FontAwesomeIcon icon={faSignOut} className="me-2" /> Logout
-              </button>
+  return myProfile && admins ? (
+    <>
+      <div className="container p-5">
+        <div className="row py-3 px-3 align-items-center">
+          <div className="col-1">
+            <div className="project--text--primary">
+              <FontAwesomeIcon icon={faUser} style={{ fontSize: 91 }} />
             </div>
           </div>
-          <div className="container">
-            <button
-              className="btn project--text--bg--primary"
-              data-bs-toggle="modal"
-              data-bs-target="#addAdminModal"
-            >
-              <FontAwesomeIcon icon={faUserPlus} className="me-2" />
-              Add Admin
+          <div className="col-7 ps-4">
+            <h1>
+              {myProfile.firstName} {myProfile.lastName}
+            </h1>
+            <p>{myProfile.email}</p>
+          </div>
+          <div className="col-4">
+            <button className="btn project--text--bg--primary me-3">
+              Edit Profile
+            </button>
+            <button className="btn btn-danger" onClick={logOut}>
+              <FontAwesomeIcon icon={faSignOut} className="me-2" /> Logout
             </button>
           </div>
-          <div className="container mt-3">
-            <h3>Admin Panel</h3>
-            <hr />
-            {admins
-              .filter((admin) => admin.id !== myProfile.id)
-              .map((admin) => {
-                return (
-                  <div className="row py-3 px-3 align-items-center">
-                    <div className="col-1">
-                      <div className="project--text--primary">
-                        <FontAwesomeIcon
-                          icon={faUser}
-                          style={{ fontSize: 30 }}
-                        />
-                      </div>
-                    </div>
-                    <div className="col-9">
-                      <h4>
-                        {admin.firstName} {admin.lastName}
-                      </h4>
-                    </div>
-                    <div className="col-2">
-                      <button
-                        className="btn project--text--bg--primary"
-                        onClick={() => removeAdmin(admin)}
-                      >
-                        Remove Admin
-                      </button>
+        </div>
+        <div className="container">
+          <button
+            className="btn project--text--bg--primary"
+            data-bs-toggle="modal"
+            data-bs-target="#addAdminModal"
+          >
+            <FontAwesomeIcon icon={faUserPlus} className="me-2" />
+            Add Admin
+          </button>
+        </div>
+        <div className="container mt-3">
+          <h3>Admin Panel</h3>
+          <hr />
+          {admins
+            .filter((admin) => admin.id !== myProfile.id)
+            .map((admin) => {
+              return (
+                <div className="row py-3 px-3 align-items-center">
+                  <div className="col-1">
+                    <div className="project--text--primary">
+                      <FontAwesomeIcon icon={faUser} style={{ fontSize: 30 }} />
                     </div>
                   </div>
-                );
-              })}
-          </div>
+                  <div className="col-9">
+                    <h4>
+                      {admin.firstName} {admin.lastName}
+                    </h4>
+                  </div>
+                  <div className="col-2">
+                    <button
+                      className="btn project--text--bg--primary"
+                      onClick={() => removeAdmin(admin)}
+                    >
+                      Remove Admin
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
         </div>
+      </div>
 
-        <AddAdminModal setAdmins={setAdmins} />
-      </>
-    )
+      <AddAdminModal setAdmins={setAdmins} />
+    </>
+  ) : (
+    <LoadingAnimation />
   );
 };
 
