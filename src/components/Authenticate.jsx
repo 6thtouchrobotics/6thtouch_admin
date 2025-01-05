ef9bc05919438d4fce05c9325575640fe1776ffb
mumuniazeez <mumuniazeez99@gmail.com>
2025-01-05 15:26:05 +0100

    `Refactored Authenticate component to display a loading animation instead of a skeleton layout`

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -26,52 +26,13 @@ const Authenticate = ({ children, setUser }) => {
   return isAuthenticated ? (
     children
   ) : (
-    <div>
-      <div className="container-fluid p-0">
-        <Skeleton width={"100%"} height={64} />
-
-        <div className="container-fluid p-0">
-          <div className="row">
-            <div className="col-2">
-              <Skeleton
-                style={{
-                  height: "100vh",
-                }}
-                width={"100%"}
-              />
-            </div>
-            <div className="col-10">
-              <div className="container p-4">
-                <Skeleton
-                  // className="container-fluid rounded-3 mt-3"
-                  width={"100%"}
-                  height={134}
-                />
-              </div>
-              <div className="container mt-5 px-5">
-                <div className="d-flex">
-                  <Skeleton
-                    className="rounded-3 d-flex align-items-center justify-content-center py-3 px-5 me-3"
-                    width={250}
-                    height={168}
-                  />
-                  <Skeleton
-                    className="rounded-3 d-flex align-items-center justify-content-center py-3 px-5 me-3"
-                    width={250}
-                    height={168}
-                  />
-                  <Skeleton
-                    className="rounded-3 d-flex align-items-center justify-content-center py-3 px-5"
-                    width={250}
-                    height={168}
-                  />
-
-                  {/* className="bg-info rounded-3 text-white  d-flex align-items-center justify-content-center py-3 px-5 me-3" */}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div
+      className="container d-flex align-items-center justify-content-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="text-center">
+        <LoadingAnimation addWhiteSpace={false} />
+        <h6>Loading Dashboard, Please Wait...</h6>
       </div>
     </div>
   );
